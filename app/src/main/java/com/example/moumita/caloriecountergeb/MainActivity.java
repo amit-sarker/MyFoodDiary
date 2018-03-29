077e0ef1197cfe0f1ab3fb688e703745df13280e
moumita.ch <2015-516-794@student.cse.du.ac.bd>
2018-03-29 19:03:53 +0600

    Initial commit

--- app/src/main/java/com/example/moumita/caloriecountergeb/MainActivity.java
+++ app/src/main/java/com/example/moumita/caloriecountergeb/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.moumita.caloriecountergeb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
