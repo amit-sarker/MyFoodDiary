93486a534879c005199d6a5a0fe0a771805da72c
moumita.ch <2015-516-794@student.cse.du.ac.bd>
2018-03-31 20:49:48 +0600

    Gender Info Activity Added

--- app/src/main/java/com/example/moumita/caloriecountergeb/MainActivity.java
+++ app/src/main/java/com/example/moumita/caloriecountergeb/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, GeneralUserActivity.class);
+        Intent intent = new Intent(MainActivity.this, UserGenderInfo.class);
         startActivity(intent);
 
     }
