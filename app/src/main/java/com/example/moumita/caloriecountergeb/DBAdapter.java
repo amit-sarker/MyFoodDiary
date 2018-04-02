81b3fbcc2748e1f66159bb53b88b60d81824053b
moumita.ch <2015-516-794@student.cse.du.ac.bd>
2018-04-02 07:10:10 +0600

    Food database table creation done. Need to update table entries.

---
+++ app/src/main/java/com/example/moumita/caloriecountergeb/DBAdapter.java
@@ -0,0 +1,193 @@
+package com.example.moumita.caloriecountergeb;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DBAdapter {
+    /* 01 Variables ---------------------------------------- */
+    private static final String databaseName = "MYDB1";
+    private static final int databaseVersion = 20;
+
+    /* 02 Database variables ------------------------------- */
+    private final Context context;
+    private DatabaseHelper DBHelper;
+    private SQLiteDatabase db;
+
+
+    /* 03 Class DbAdapter ---------------------------------- */
+    public DBAdapter(Context ctx){
+        this.context = ctx;
+        DBHelper = new DatabaseHelper(context);
+    }
+
+    /* 04 DatabaseHelper ------------------------------------ */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context){
+            super(context, databaseName, null, databaseVersion);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            /*try{
+                // Create tables
+                db.execSQL("CREATE TABLE IF NOT EXISTS users (" +
+                        " user_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        " user_email VARCHAR," +
+                        " user_password VARCHAR, " +
+                        " user_salt VARCHAR, " +
+                        " user_alias VARCHAR," +
+                        " user_dob DATE, " +
+                        " user_gender INT, " +
+                        " user_location VARHCAR, " +
+                        " user_height INT, " +
+                        " user_activity_level INT, " +
+                        " user_weight INT, " +
+                        " user_target_weight INT, " +
+                        " user_target_weight_level INT," +
+                        " user_last_seen TIME," +
+                        " user_note VARCHAR);");
+
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }*/
+
+            try{
+                db.execSQL("CREATE TABLE IF NOT EXISTS food_diary_cal_eaten (" +
+                        " cal_eaten_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        " cal_eaten_date DATE, " +
+                        " cal_eaten_meal_no INT, " +
+                        " cal_eaten_energy INT, " +
+                        " cal_eaten_proteins INT, " +
+                        " cal_eaten_carbs INT, " +
+                        " cal_eaten_fat INT);");
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try{
+                db.execSQL("CREATE TABLE IF NOT EXISTS food_diary (" +
+                        " fd_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        " fd_date DATE," +
+                        " fd_meal_number INT," +
+                        " fd_food_id INT," +
+                        " fd_serving_size DOUBLE," +
+                        " fd_serving_mesurment VARCHAR," +
+                        " fd_energy_calculated DOUBLE," +
+                        " fd_protein_calculated DOUBLE," +
+                        " fd_carbohydrates_calculated DOUBLE," +
+                        " fd_fat_calculated DOUBLE," +
+                        " fd_fat_meal_id INT);");
+
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try{
+                db.execSQL("CREATE TABLE IF NOT EXISTS categories (" +
+                        " category_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        " category_name VARCHAR," +
+                        " category_parent_id INT," +
+                        " category_icon VARCHAR," +
+                        " category_note VARCHAR);");
+
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                db.execSQL("CREATE TABLE IF NOT EXISTS food (" +
+                        " food_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        " food_name VARCHAR," +
+                        " food_manufactor_name VARCHAR," +
+                        " food_serving_size DOUBLE," +
+                        " food_serving_mesurment VARCHAR," +
+                        " food_serving_name_number DOUBLE," +
+                        " food_serving_name_word VARCHAR," +
+                        " food_energy DOUBLE," +
+                        " food_proteins DOUBLE," +
+                        " food_carbohydrates DOUBLE," +
+                        " food_fat DOUBLE," +
+                        " food_energy_calculated DOUBLE," +
+                        " food_proteins_calculated DOUBLE," +
+                        " food_carbohydrates_calculated DOUBLE," +
+                        " food_fat_calculated DOUBLE," +
+                        " food_user_id INT," +
+                        " food_barcode VARCHAR," +
+                        " food_category_id INT," +
+                        " food_thumb VARCHAR," +
+                        " food_image_a VARCHAR," +
+                        " food_image_b VARCHAR," +
+                        " food_image_c VARCHAR," +
+                        " food_notes VARCHAR);");
+
+
+
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+
+            // ! All tables that are going to be dropped need to be listed here
+
+            //db.execSQL("DROP TABLE IF EXISTS users");
+            db.execSQL("DROP TABLE IF EXISTS food_diary_cal_eaten");
+            db.execSQL("DROP TABLE IF EXISTS food_diary");
+            db.execSQL("DROP TABLE IF EXISTS categories");
+            db.execSQL("DROP TABLE IF EXISTS food");
+            onCreate(db);
+
+            String TAG = "Tag";
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+
+        } // end public void onUpgrade
+    } // DatabaseHelper
+
+
+    /* 05 Open database --------------------------------------------------------- */
+    public DBAdapter open() throws SQLException {
+        db = DBHelper.getWritableDatabase();
+        return this;
+    }
+
+    /* 06 Close database --------------------------------------------------------- */
+    public void close() {
+        DBHelper.close();
+    }
+
+    /* 07 Insert data ------------------------------------------------------------ */
+    public void insert(String table, String fields, String values){
+        db.execSQL("INSERT INTO " + table +  "(" + fields + ") VALUES (" + values + ")");
+    }
+
+    /* 08 Count ------------------------------------------------------------------ */
+    public int count(String table)
+    {
+        try {
+            Cursor mCount = db.rawQuery("SELECT COUNT(*) FROM " + table + "", null);
+            mCount.moveToFirst();
+            int count = mCount.getInt(0);
+            mCount.close();
+            return count;
+        }
+        catch(SQLiteException e){
+            return -1;
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/moumita/caloriecountergeb/MainActivity.java
+++ app/src/main/java/com/example/moumita/caloriecountergeb/MainActivity.java
@@ -1,25 +1,53 @@
 package com.example.moumita.caloriecountergeb;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 
-import userinfo.UserGenderInfoActivity;
+import okhttp3.OkHttpClient;
+
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Person User = new Person();
-        Intent intent = new Intent(MainActivity.this, UserGenderInfoActivity.class);
-        //intent.putExtra("sampleObject", (Serializable) User);
-        startActivity(intent);
+        /* Stetho */
+        Stetho.initializeWithDefaults(this);
+
+        new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+
+        /* Database */
+        DBAdapter db = new DBAdapter(this);
+        db.open();
+
+        /* Setup for food */
+        // Count rows in food
+        int numberRows = db.count("food");
+
+        if(numberRows < 1){
+            // Run setup
+            Toast.makeText(this, "Loading setup...", Toast.LENGTH_LONG).show();
+            DBSetupInsert setupInsert = new DBSetupInsert(this);
+            setupInsert.insertAllCategories();
+            setupInsert.insertAllFood();
+            Toast.makeText(this, "Setup completed!", Toast.LENGTH_LONG).show();
+
+        }
+
+
+
+        /* Close database */
+        db.close();
+
+
 
     }
 }
