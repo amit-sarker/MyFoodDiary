abdc8d052fe710c74416a61018047053c56b67f0
moumita.ch <2015-516-794@student.cse.du.ac.bd>
2018-04-20 14:40:48 +0600

    breakfast, lunch, dinner cardview added in homefragment

--- app/src/main/java/fragments/HomeFragment.java
+++ app/src/main/java/fragments/HomeFragment.java
@@ -15,6 +15,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.example.moumita.caloriecountergeb.AddFoodActivity;
@@ -44,11 +46,12 @@ public class HomeFragment extends Fragment {
     private FitChart fitChart;
     private Button addButton;
     private PersonOperations personData;
-    private ArrayList<ShowFood> showFoodModels;
-    private ArrayList<InitialShowFood> initialShowFoodModels;
-    private ListView listView;
+    private ArrayList<ShowFood> showBreakfastModels, showLunchModels, showDinnerModels;
+    private ArrayList<InitialShowFood> initialShowBreakfastModels, initialShowLunchModels, initialShowDinnerModels;
+    private ListView breakfastListView, lunchListView, dinnerListView;
     private static ShowFoodAdapter showFoodAdapter;
     private static InitialShowFoodAdapter initialShowFoodAdapter;
+    private int listviewsize = 100;
 
 
     @Override
@@ -111,25 +114,34 @@ public class HomeFragment extends Fragment {
 
         //Adding Listview
 
-        listView=view.findViewById(R.id.show_food_list);
+        breakfastListView=view.findViewById(R.id.show_breakfast_list);
+        lunchListView=view.findViewById(R.id.show_lunch_list);
+        dinnerListView=view.findViewById(R.id.show_dinner_list);
 
 
-        showFoodModels= new ArrayList<>();
-        initialShowFoodModels = new ArrayList<>();
-
-        //showFoodModels.add(new ShowFood(R.drawable.ic_favorite, "Apple", "1200 kcal", "5 cup (400gm)"));
 
 
-        initialShowFoodModels.add(new InitialShowFood(R.drawable.breakfast,"Eat breakfast, Start Healthy Life"));
-        if(showFoodModels.isEmpty()==true)
+        showBreakfastModels= new ArrayList<>();
+        initialShowBreakfastModels = new ArrayList<>();
+        showLunchModels = new ArrayList<>();
+        initialShowLunchModels = new ArrayList<>();
+        showDinnerModels = new ArrayList<>();
+        initialShowDinnerModels = new ArrayList<>();
+
+        showBreakfastModels.add(new ShowFood(R.drawable.food5, "Apple", "1200 kcal", "5 cup (400gm)"));
+        showBreakfastModels.add(new ShowFood(R.drawable.food2, "Noodles", "450 kcal", "1 plate"));
+
+
+        initialShowBreakfastModels.add(new InitialShowFood(R.drawable.breakfast,"Eat breakfast, Start Healthy Life"));
+        if(showBreakfastModels.isEmpty()==true)
         {
-            initialShowFoodAdapter= new InitialShowFoodAdapter(initialShowFoodModels,getContext());
+            initialShowFoodAdapter= new InitialShowFoodAdapter(initialShowBreakfastModels,getContext());
 
-            listView.setAdapter(initialShowFoodAdapter);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            breakfastListView.setAdapter(initialShowFoodAdapter);
+            breakfastListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    InitialShowFood initialShowFood= initialShowFoodModels.get(position);
+                    InitialShowFood initialShowFood= initialShowBreakfastModels.get(position);
 
                 }
             });
@@ -137,21 +149,118 @@ public class HomeFragment extends Fragment {
         }
         else
         {
-            showFoodAdapter= new ShowFoodAdapter(showFoodModels,getContext());
+            showFoodAdapter= new ShowFoodAdapter(showBreakfastModels,getContext());
 
-            listView.setAdapter(showFoodAdapter);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            breakfastListView.setAdapter(showFoodAdapter);
+            breakfastListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    ShowFood showFoodModel= showFoodModels.get(position);
+                    ShowFood showFoodModel= showBreakfastModels.get(position);
 
                 }
             });
         }
 
 
+        initialShowLunchModels.add(new InitialShowFood(R.drawable.lunch,"Take a break, Take lunch"));
+        if(showLunchModels.isEmpty()==true)
+        {
+            initialShowFoodAdapter= new InitialShowFoodAdapter(initialShowLunchModels,getContext());
+
+            lunchListView.setAdapter(initialShowFoodAdapter);
+            lunchListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    InitialShowFood initialShowFood= initialShowLunchModels.get(position);
+
+                }
+            });
+
+        }
+        else
+        {
+            showFoodAdapter= new ShowFoodAdapter(showLunchModels,getContext());
+
+            lunchListView.setAdapter(showFoodAdapter);
+            lunchListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    ShowFood showFoodModel= showLunchModels.get(position);
+
+                }
+            });
+        }
+
+
+        initialShowDinnerModels.add(new InitialShowFood(R.drawable.dinner,"End of the day? Gift yourself a wonderful dinner"));
+        if(showLunchModels.isEmpty()==true)
+        {
+            initialShowFoodAdapter= new InitialShowFoodAdapter(initialShowDinnerModels,getContext());
+
+            dinnerListView.setAdapter(initialShowFoodAdapter);
+            dinnerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    InitialShowFood initialShowFood= initialShowDinnerModels.get(position);
+
+                }
+            });
+
+        }
+        else
+        {
+            showFoodAdapter= new ShowFoodAdapter(showDinnerModels,getContext());
+
+            dinnerListView.setAdapter(showFoodAdapter);
+            dinnerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    ShowFood showFoodModel= showDinnerModels.get(position);
+
+                }
+            });
+        }
+
+        setListViewHeightBasedOnItems(breakfastListView);
+        setListViewHeightBasedOnItems(lunchListView);
+        setListViewHeightBasedOnItems(dinnerListView);
 
         return view;
+
+    }
+
+    public static boolean setListViewHeightBasedOnItems(ListView listView) {
+
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter != null) {
+
+            int numberOfItems = listAdapter.getCount();
+
+            // Get total height of all items.
+            int totalItemsHeight = 0;
+            for (int itemPos = 0; itemPos < numberOfItems; itemPos++) {
+                View item = listAdapter.getView(itemPos, null, listView);
+                float px = 500 * (listView.getResources().getDisplayMetrics().density);
+                item.measure(View.MeasureSpec.makeMeasureSpec((int)px, View.MeasureSpec.AT_MOST), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+                totalItemsHeight += item.getMeasuredHeight();
+            }
+
+            // Get total height of all item dividers.
+            int totalDividersHeight = listView.getDividerHeight() *
+                    (numberOfItems - 1);
+            // Get padding
+            int totalPadding = listView.getPaddingTop() + listView.getPaddingBottom();
+
+            // Set list height.
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = totalItemsHeight + totalDividersHeight + totalPadding;
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+            return true;
+
+        } else {
+            return false;
+        }
 
     }
 
